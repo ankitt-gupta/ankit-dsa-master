f3afbc50ea93ee7c73402271b2edabe1dd16050f
ankitt-gupta <ankit.ag1782@gmail.com>
2024-03-11 23:22:58 +0530

    Initial commit: Adding folders for different DSAs chapters in main branch

---
+++ searching-dsa/src/com/ankit/dsa/master/searching/SearchingDsaMain.java
@@ -0,0 +1,9 @@
+package com.ankit.dsa.master.searching;
+
+public class SearchingDsaMain {
+
+  public static void main(String[] args) {
+    System.out.println("Hello! Searching DSA");
+  }
+
+}
---
+++ sorting-dsa/src/com/ankit/dsa/master/sorting/SortingDsaMain.java
@@ -0,0 +1,9 @@
+package com.ankit.dsa.master.sorting;
+
+public class SortingDsaMain {
+
+  public static void main(String[] args) {
+    System.out.println("Hello! Sorting DSA");
+  }
+
+}
