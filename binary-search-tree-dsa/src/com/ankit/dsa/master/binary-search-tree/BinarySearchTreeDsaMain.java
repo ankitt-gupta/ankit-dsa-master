9b5a378b67a33828019ced8d49b1ad0f9de11e2e
ankitt-gupta <ankit.ag1782@gmail.com>
2024-03-13 01:08:59 +0530

    Initial commit: binary-search-tree-dsa folder added

---
+++ binary-search-tree-dsa/src/com/ankit/dsa/master/binary-search-tree/BinarySearchTreeDsaMain.java
@@ -0,0 +1,9 @@
+package com.ankit.dsa.master.binary-search-tree;
+
+public class BinarySearchTreeDsaMain {
+
+  public static void main(String[] args) {
+    System.out.println("Hello! binary-search-tree DSA");
+  }
+
+}
