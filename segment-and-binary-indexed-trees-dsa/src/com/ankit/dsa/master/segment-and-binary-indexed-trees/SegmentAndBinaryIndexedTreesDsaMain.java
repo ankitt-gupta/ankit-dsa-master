101f420103340264452c22f83219840724b14139
ankitt-gupta <ankit.ag1782@gmail.com>
2024-03-13 11:45:52 +0530

    Initial commit: segment-and-binary-indexed-trees-dsa folder added

---
+++ segment-and-binary-indexed-trees-dsa/src/com/ankit/dsa/master/segment-and-binary-indexed-trees/SegmentAndBinaryIndexedTreesDsaMain.java
@@ -0,0 +1,9 @@
+package com.ankit.dsa.master.segment-and-binary-indexed-trees;
+
+public class SegmentAndBinaryIndexedTreesDsaMain {
+
+  public static void main(String[] args) {
+    System.out.println("Hello! Segment And Binary Indexed Trees DSA");
+  }
+
+}
