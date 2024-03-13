7bf69ae44b7aaba614e7b9614c9365500e89dc08
ankitt-gupta <ankit.ag1782@gmail.com>
2024-03-13 11:47:57 +0530

    Initial commit: disjoint-set-dsa folder added

---
+++ disjoint-set-dsa/src/com/ankit/dsa/master/disjoint-set/DisjointSetDsaMain.java
@@ -0,0 +1,9 @@
+package com.ankit.dsa.master.disjoint-set;
+
+public class DisjointSetDsaMain {
+
+  public static void main(String[] args) {
+    System.out.println("Hello! Disjoint Set DSA");
+  }
+
+}
