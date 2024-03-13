804751e494cf5f09f3bd7d77f1f4e63bab918f3f
ankitt-gupta <ankit.ag1782@gmail.com>
2024-03-13 11:50:57 +0530

    Initial commit: mathamatics-dsa folder added

---
+++ mathamatics-dsa/src/com/ankit/dsa/master/mathamatics/MathamaticsDsaMain.java
@@ -0,0 +1,9 @@
+package com.ankit.dsa.master.mathamatics;
+
+public class MathamaticsDsaMain {
+
+  public static void main(String[] args) {
+    System.out.println("Hello! Mathamatics DSA");
+  }
+
+}
