27efa654f6e339a2352f58b66ddaadc39d872bdd
ankitt-gupta <ankit.ag1782@gmail.com>
2025-06-28 01:07:32 +0530

    The Class Array problems: Max and Second Max

--- arrays-dsa/src/com/ankit/dsa/master/arrays/ArrayProblems.java
+++ arrays-dsa/src/com/ankit/dsa/master/arrays/ArrayProblems.java
@@ -1,5 +1,8 @@
 package com.ankit.dsa.master.arrays;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * The Class Array problems.
  * 
@@ -13,12 +16,12 @@ public class ArrayProblems {
 
 //1. Maximum Index
 
-		//Input: {34, 8, 10, 3, 2, 80, 30, 33, 1}
-		//Expected: 6
-		
-		int[] a = {34, 8, 10, 3, 2, 80, 30, 33, 1} ;
-		
- 		int lMin[] = new int[a.length];
+		// Input: {34, 8, 10, 3, 2, 80, 30, 33, 1}
+		// Expected: 6
+
+		int[] a = { 34, 8, 10, 3, 2, 80, 30, 33, 1 };
+
+		int lMin[] = new int[a.length];
 		int rMax[] = new int[a.length];
 		if (a.length == 1) {
 			System.out.println("Max Difference : " + 0);
@@ -35,17 +38,39 @@ public class ArrayProblems {
 		}
 
 		int maxDiff = -1;
-		int i = 0;
-		int j = 0;
-		while ((i < a.length) && (j < a.length)) {
-			if (lMin[i] <= rMax[j]) {
-				maxDiff = Math.max(maxDiff, j - i);
-				j++;
+		int i1 = 0;
+		int j1 = 0;
+		while ((i1 < a.length) && (j1 < a.length)) {
+			if (lMin[i1] <= rMax[j1]) {
+				maxDiff = Math.max(maxDiff, j1 - i1);
+				j1++;
 			} else {
-				i++;
+				i1++;
 			}
 		}
 		System.out.println("Max difference : " + maxDiff);
+
+//2. Max and Second Max
+
+		// Input:
+		// Expected:
+		int a2[] = {};
+		int sizeOfArray = a2.length;
+		int secondMax = -1;
+		int max = a2[0];
+		if (sizeOfArray == 1) {
+			System.out.println(" Max and Second Max : " + new ArrayList<>(Arrays.asList(a[0], -1)));
+		}
+		for (int i = 1; i < sizeOfArray; i++) {
+			if (a[i] > max) {
+				secondMax = max;
+				max = a[i];
+			} else if (secondMax < a[i] && max != a[i]) {
+				secondMax = a[i];
+			}
+		}
+
+		System.out.println(" Max and Second Max : " + new ArrayList<>(Arrays.asList(max, secondMax)));
 
 	}
 
